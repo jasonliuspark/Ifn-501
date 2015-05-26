6843c627f62014152168ef5078d0fe244d30934b
jasonliuspark <jasonliuspark@gmail.com>
2015-05-26 14:52:23 +1000

    10

--- third/n9199349_assignment2/Forms/ProductInventory.cs
+++ third/n9199349_assignment2/Forms/ProductInventory.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Collections;
+using System.Xml;
 
 namespace n9199349_assignment2
 {
---
+++ third/n9199349_assignment2/xml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace n9199349_assignment2
+{
+  public class xml
+    {  
+        string MyXmlfilePath = "c:\\Inventory_output.xml";
+        public void xml(int x,string y)
+        { 
+          XmlDocument myXmlDoc=new XmlDocument();
+          XmlElement rootElement = myXmlDoc.CreateElement("Products");
+          myXmlDoc.AppendChild(rootElement);
+       
+          XmlElement [] LevelElement1= new XmlElement();
+
+          //LevelElement1[x].SetAttribute("Products",);
+        }
+       public  void genXML(int x,int y,string a,string b)
+       {
+          
+     
+
+          
+       
+       
+       }
+
+   
+  
+  
+  }
+}
